8b1cc1ea8825c2f2d139286bedd76825d3bd797c
Allendra Donny Irawan <105181500+VerzF@users.noreply.github.com>
2024-05-19 15:54:42 +0700

    Initial commit

---
+++ app/src/main/java/com/trisulaforce/laundryapp/model/item.kt
@@ -0,0 +1,10 @@
+package com.trisulaforce.laundryapp.model
+
+data class Item (
+    val id: Long,
+    val name: String,
+    val photoUrl: Int,
+    val description: String,
+    val price: Int
+)
+
---
+++ app/src/main/java/com/trisulaforce/laundryapp/model/itemData.kt
@@ -0,0 +1,232 @@
+package com.trisulaforce.laundryapp.model
+
+import com.trisulaforce.laundryapp.R
+
+object ItemData {
+    val items = listOf(
+        Item(
+            1,
+            "Kiloan Reguler (Min. 2 Kg)",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            2,
+            "Kiloan Reguler Balita(Min. 2 Kg)",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            3,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            4,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            5,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            6,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            7,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            8,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            9,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            10,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            11,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            12,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            13,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            14,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            15,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            16,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            17,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            18,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            19,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            20,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            21,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            22,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            23,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            24,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            25,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            26,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            27,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            28,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            29,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            30,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            31,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+        Item(
+            32,
+            "Kopi 1",
+            R.drawable.a1,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. .",
+            5000
+        ),
+    )
+}
